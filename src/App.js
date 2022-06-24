2adeff64c1084a0a7619d5b5922831d9e15b6a73
Folareen <sakawahab03@gmail.com>
2022-06-25 00:23:02 +0100

    structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import Hero from './components/Footer'
+import Main from './components/Main'
+import Cta from './components/Cta'
+import Footer from './components/Footer'
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-red-300'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hero />
+      <Main />
+      <Cta />
+      <Footer />
+    </>
   );
 }
 
