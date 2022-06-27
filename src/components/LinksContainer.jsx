ad81a815223a8135d675e8083c247a6a2db1c02e
Folareen <sakawahab03@gmail.com>
2022-06-27 01:38:19 +0100

    links shortening

--- src/components/LinksContainer.jsx
+++ src/components/LinksContainer.jsx
@@ -10,13 +10,14 @@ const LinksContainer = () => {
     try{
       const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`)
       const data = await res.json()
-      const newLink = data.result.short_link
+      const newLink = data.result
       // .short_link
       setLinks([...links, newLink])
       setLink("")
       setError(false)
       setLoading(false)
-      console.log(links)
+      console.log(newLink)
+      // .short_link
     }
     catch{
       setError(true)
@@ -32,7 +33,7 @@ const LinksContainer = () => {
     <div className="flex flex-col mb-6 -mt-12 ">
       <form action="" className="bg-DarkViolet px-5 py-8 flex justify-between rounded-md relative">
         <input type="text" name="" value={link} onChange={changeLink} id="" placeholder='Shorten a link here...' className={`bg-white rounded-md px-4 py-2 w-3/6 ${error ? "border-2 border-rose-500" : "" }` }/>
-        <button type="submit" className={`px-4 py-2 text-white rounded-md font-bold ${loading ? "bg-cyan-300" : "bg-Cyan"}`} onClick={(e) => {
+        <button type="submit" className={`px-4 py-2 text-white rounded-md font-bold ${loading ? "bg-cyan-200" : "bg-Cyan"}`} onClick={(e) => {
           e.preventDefault();
           if(link.length > 1){
             setError(false)
@@ -61,12 +62,13 @@ const LinksContainer = () => {
         (
         links.map(
           (item) =>{
+            const {short_link, original_link} = item
             return <div className='bg-white mt-5 rounded-md flex flex-col p-3 text-left'>
               <a href='' className="border-b-2 border-DarkViolet py-1">
-                {item}
+                {original_link}
               </a>
               <a href='' className='py-1 text-Cyan font-semibold'>
-              {item}
+              {short_link}
               </a>
               <button className='bg-Cyan rounded-lg py-2 text-white font-bold '>
                 Copy
